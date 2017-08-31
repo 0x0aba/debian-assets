dcb8fe38c48f2933ad91dabd37171c67c69909f5
Michael Stapelberg <michael@stapelberg.de>
2017-08-31 08:19:19 +0200

    update scripts from manziarly.debian.org

--- scripts/mirror-push.bash
+++ scripts/mirror-push.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "$(date)" >> /tmp/mirror-push.debug.log
+# Rotate old logs out of the way
+/usr/sbin/logrotate -f -s /srv/manpages.debian.org/debiman/logs/logrotate.status /srv/manpages.debian.org/debiman/logs/logrotate.conf
+
+# Redirect stdout and stderr to the current logfiles
+exec >/srv/manpages.debian.org/debiman/logs/stdout 2>/srv/manpages.debian.org/debiman/logs/stderr
+
+# Run debiman
+/srv/manpages.debian.org/debiman/run-debiman.bash
+
+# Distribute the result to the static mirroring infrastructure.
+static-update-component manpages.debian.org
+
+# Distribute the auxserver index to dyn.manpages.debian.org hosts.
+rsync /srv/manpages.debian.org/www/auxserver.idx cgi-grnet-01.debian.org:/srv/manpages.debian.org/auxserver.idx
--- scripts/run-debiman.bash
+++ scripts/run-debiman.bash
@@ -5,6 +5,18 @@ set -e
 
 (cd /srv/manpages.debian.org/debiman/debian-assets && git pull)
 
+# Try to update the piuparts data files, but continue if they are not
+# available.
+(wget --ca-certificate=/etc/ssl/ca-debian/ca-certificates.crt https://piuparts.debian.org/for-manpages.d.o/stretch.json.gz -O /srv/manpages.debian.org/debiman/piuparts/.stretch.json.gz && mv /srv/manpages.debian.org/debiman/piuparts/.stretch.json.gz /srv/manpages.debian.org/debiman/piuparts/stretch.json.gz) || true
+
+(wget --ca-certificate=/etc/ssl/ca-debian/ca-certificates.crt https://piuparts.debian.org/for-manpages.d.o/jessie.json.gz -O /srv/manpages.debian.org/debiman/piuparts/.jessie.json.gz && mv /srv/manpages.debian.org/debiman/piuparts/.jessie.json.gz /srv/manpages.debian.org/debiman/piuparts/jessie.json.gz) || true
+
+(wget --ca-certificate=/etc/ssl/ca-debian/ca-certificates.crt https://piuparts.debian.org/for-manpages.d.o/sid.json.gz -O /srv/manpages.debian.org/debiman/piuparts/.unstable.json.gz && mv /srv/manpages.debian.org/debiman/piuparts/.unstable.json.gz /srv/manpages.debian.org/debiman/piuparts/unstable.json.gz) || true
+
+(wget --ca-certificate=/etc/ssl/ca-debian/ca-certificates.crt https://piuparts.debian.org/for-manpages.d.o/experimental.json.gz -O /srv/manpages.debian.org/debiman/piuparts/.experimental.json.gz && mv /srv/manpages.debian.org/debiman/piuparts/.experimental.json.gz /srv/manpages.debian.org/debiman/piuparts/experimental.json.gz) || true
+
+(wget --ca-certificate=/etc/ssl/ca-debian/ca-certificates.crt https://piuparts.debian.org/for-manpages.d.o/buster.json.gz -O /srv/manpages.debian.org/debiman/piuparts/.testing.json.gz && mv /srv/manpages.debian.org/debiman/piuparts/.testing.json.gz /srv/manpages.debian.org/debiman/piuparts/testing.json.gz) || true
+
 ulimit -c unlimited
 PATH=/srv/manpages.debian.org/debiman/mandoc-batch:$PATH \
 TMPDIR=/srv/manpages.debian.org/tmp/ \
@@ -13,13 +25,14 @@ TMPDIR=/srv/manpages.debian.org/tmp/ \
   ionice -n 7 \
   /srv/manpages.debian.org/debiman/gopath/bin/debiman \
   -inject_assets=/srv/manpages.debian.org/debiman/debian-assets \
+  -alternatives_dir=/srv/manpages.debian.org/debiman/piuparts/ \
   -sync_codenames=oldstable,oldstable-backports,stable,stable-backports \
   -sync_suites=testing,unstable,experimental \
   -serving_dir=/srv/manpages.debian.org/www \
   -local_mirror=/srv/mirrors/debian $@
 
 # reload the index
-killall -HUP debiman-auxserver
+pkill -u manpages -HUP -f debiman-auxserver
 
 # convert the index
 TMPDIR=$(mktemp -d -p /srv/manpages.debian.org/tmp rwmap-tmpXXXXXX)
@@ -34,7 +47,7 @@ LC_ALL=C sort ${TMPDIR}/output.* > ${TMPDIR}/rwmap.txt
 # the file format, but will respect the file format of an already existing
 # output file.
 echo -n H4sICEEvilgAA2VtcHR5My5kYm0A7dexCYBADIXhF+GK624DLVxAbJzBMVzBNRzO0tpJPDnlRLEW4f8gJCRkgCdJpmRonPw+hFg+7c7b1dguqmPv+nmdyrwvjl69/AEAAAAAgO+YHnk9mMu3O/I/AAAAAAD/sgEYIbKQACAAAA== | base64 -d | gunzip -c > ${TMPDIR}/rwmap.dbm
-httxt2dbm -f DB -i ${TMPDIR}/rwmap.txt -o ${TMPDIR}/rwmap.dbm
+/usr/sbin/httxt2dbm -f DB -i ${TMPDIR}/rwmap.txt -o ${TMPDIR}/rwmap.dbm
 # Currently unused, but would be better to keep around and sync once the static
 # mirror infrastructure supports post-processing:
 # mv ${TMPDIR}/rwmap.txt /srv/manpages.debian.org/www/rwmap.txt
